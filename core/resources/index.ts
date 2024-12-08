80dad4005558f3f21751a80f462ce3d323c843e0
Duc Nguyen <minhduc140583@gmail.com>
2024-12-08 23:19:20 +0700

    Remove unused libraries

--- core/resources/index.ts
+++ core/resources/index.ts
@@ -1,10 +1,16 @@
-import { Resources, StringMap } from "uione"
 import { en as adminEN } from "./admin/en"
 import { vi as adminVI } from "./admin/vi"
 import { en as authenticationEN } from "./authentication/en"
 import { vi as authenticationVI } from "./authentication/vi"
 import { en as commonEN } from "./en"
 import { vi as commonVI } from "./vi"
+
+export interface Resources {
+  [key: string]: StringMap
+}
+export interface StringMap {
+  [key: string]: string
+}
 
 const en: StringMap = {
   ...commonEN,
