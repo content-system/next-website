5980773aadd436dd602353735f5d14cf1895ab8b
Duc Nguyen <minhduc140583@gmail.com>
2025-11-08 01:29:06 +0700

    Upgrade authen-service

--- service/index.ts
+++ service/index.ts
@@ -1,6 +1,5 @@
 import { Authenticator, initializeStatus, SqlAuthTemplateConfig, User, useUserRepository } from "authen-service"
 import { compare } from "bcrypt"
-import { generateToken } from "jsonwebtoken-plus"
 import { StringMap } from "onecore"
 import { DB } from "query-core"
 import { ArticleService, useArticleService } from "./article"
@@ -21,17 +20,6 @@ export function useContext(db: DB, auth: SqlAuthTemplateConfig, map: StringMap):
 
   const status = initializeStatus(auth.status)
   const userRepository = useUserRepository<string, SqlAuthTemplateConfig>(db, auth, map)
-  const authenticator = new Authenticator(
-    status,
-    compare,
-    generateToken,
-    auth.token,
-    auth.payload,
-    auth.account,
-    userRepository,
-    undefined,
-    auth.lockedMinutes,
-    auth.maxPasswordFailed,
-  )
+  const authenticator = new Authenticator(status, compare, auth.account, userRepository, undefined, auth.lockedMinutes, auth.maxPasswordFailed)
   return { article, job, contact, authenticator }
 }
