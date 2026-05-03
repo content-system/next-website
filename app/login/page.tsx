9719a374a84bdfa05c9ba5b4829dbc4deefcea57
Duc Nguyen <minhduc140583@gmail.com>
2026-05-03 19:49:24 +0700

    Fix menu after login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,13 @@
 import { getAuthenticator } from "@lib/authentication"
 import { getResource } from "@resources"
+import { sign } from "jsonwebtoken"
+import { cookies } from "next/headers"
 import Link from "next/link"
 import { redirect } from "next/navigation"
 import { Attributes, StringMap } from "onecore"
 import { validate } from "validation-core"
 import { fromFormData } from "web-one"
+import { config } from "../../config"
 
 export const userModel: Attributes = {
   username: {
@@ -33,12 +36,9 @@ export const map: StringMap = {
   "9": "fail_disabled_account",
 }
 
-export default async function Login({ searchParams }: { searchParams: Promise<Record<string, string | string[] | undefined>> }) {
+export default async function Login({ searchParams }: { searchParams: Promise<{ username?: string; message?: string }> }) {
   const resource = getResource()
   const query = await searchParams
-  let user = {} as User
-  user.username = query.username as string
-  user.message = query.message as string
 
   async function login(formData: FormData) {
     "use server"
@@ -52,6 +52,28 @@ export default async function Login({ searchParams }: { searchParams: Promise<Re
       const result = await service.authenticate(obj)
       console.log("Result " + result.status)
       if (result.status === 1) {
+        const account = result.user
+        if (account) {
+          const cookie = await cookies()
+          if (!account.displayName) {
+            account.displayName = account.username ? account.username : account.email ? account.email : account.id
+          }
+          const secret = process.env.TOKEN_SECRET || config.token.secret
+          const token = sign(
+            {
+              id: account.id, 
+              username: obj.username,
+              displayName: account.displayName,
+              language: account.language,
+              dateFormat: account.dateFormat 
+            },
+            secret,
+            {
+              expiresIn: config.token.expires,
+            },
+          )
+          cookie.set("token", token, { httpOnly: true, path: "/" })
+        }
         redirect("/news")
       } else {
         let key: string | undefined = map["" + result.status]
@@ -60,24 +82,25 @@ export default async function Login({ searchParams }: { searchParams: Promise<Re
       }
     }
   }
+
   return (
     <div className="central-full">
       <form id="signinForm" name="signinForm" className="form" noValidate={true} autoComplete="off" action={login}>
         <div className="view-body row">
           <img className="logo" src="/logo192.png" alt="logo" />
           <h1>{resource.signin}</h1>
-          <div className="message alert-error">{user.message}</div>
+          <div className="message alert-error">{query.message}</div>
           <label className="col s12">
             {resource.username}
-            <input type="text" id="username" name="username" defaultValue={user.username} maxLength={100} placeholder={resource.placeholder_username} />
+            <input type="text" id="username" name="username" defaultValue={query.username} maxLength={100} placeholder={resource.placeholder_username} />
           </label>
           <label className="col s12">
             {resource.password}
-            <input type="password" id="password" name="password" defaultValue={user.password} maxLength={100} placeholder={resource.placeholder_password} />
+            <input type="password" id="password" name="password" maxLength={100} placeholder={resource.placeholder_password} />
           </label>
           <label className="col s12" hidden>
             {resource.passcode}
-            <input type="password" id="passcode" name="passcode" value={user.passcode} maxLength={10} placeholder={resource.placeholder_passcode} />
+            <input type="password" id="passcode" name="passcode" maxLength={10} placeholder={resource.placeholder_passcode} />
           </label>
           <label className="col s12 checkbox-container">
             <input type="checkbox" id="remember" name="remember" />
